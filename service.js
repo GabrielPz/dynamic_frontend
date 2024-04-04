cdcc9702e7103b3637f8c74796ae303837a98d9f
Gabriel <gabrielhenriquedps@gmail.com>
2024-04-03 21:59:44 -0300

    feat: add service

---
+++ service.js
@@ -0,0 +1,14 @@
+const url = process.env.BASE_URL
+
+const createTodo = async () => {
+
+    const todo = {
+        title: "atitutlo"
+    }
+
+    try{
+
+    }catch(err){
+        console.log(err)
+    }
+}
