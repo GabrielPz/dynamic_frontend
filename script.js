57d2c4fbddbe0ec09378b4b62d19f24c4fc72fef
Gabriel <gabrielhenriquedps@gmail.com>
2024-04-05 19:20:49 -0300

    feat: Criação das Tarefas

--- script.js
+++ script.js
@@ -1,6 +1,88 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+const url = 'https://crudcrud.com/api/bf82942d809d42c292b7f93e6e2f8f26/todo';
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+const handleCreateTodo = async () => {
+  const titulo = document.getElementById('tituloTarefa').value;
+  const categoria = document.getElementById('categoriaTarefa').value;
+  const hora = document.getElementById('horaTarefa').value;
+  
+  await createTodo(titulo, categoria, hora);
+  
+  const modalElement = document.getElementById('exampleModal');
+  const modalInstance = bootstrap.Modal.getInstance(modalElement);
+  modalInstance.hide();
+
+  document.getElementById('tituloTarefa').value = '';
+  document.getElementById('categoriaTarefa').value = '';
+  document.getElementById('horaTarefa').value = '';
+}
+
+
+const createTodo = async (title, category, dateTime) => {
+    const todo = {
+        title: title,
+        category: category,
+        dateTime: dateTime,
+        done: false
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(todo),
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+const readTodos = async () => {
+  try {
+      const response = await fetch(url);
+      const todos = await response.json();
+      console.log(todos);
+  } catch(err) {
+      console.error(err);
+  }
+};
+
+const readById = async (id) => {
+  try {
+      const response = await fetch(`${url}/${id}`)
+      const todos = await response.json();
+      console.log(todos)
+  } catch(err) {
+      console.error(err);
+  }
+};
+
+const updateTodo = async (id, updatedTodo) => {
+  try {
+      const response = await fetch(`${url}/${id}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedTodo),
+      });
+      const data = await response.json();
+      console.log(data);
+  } catch(err) {
+      console.error(err);
+  }
+};
+
+const deleteTodo = async (id) => {
+  try {
+      await fetch(`${url}/${id}`, {
+          method: 'DELETE',
+      });
+      console.log('Deleted:', id);
+  } catch(err) {
+      console.error(err);
+  }
+};
--- service.js
+++ service.js
@@ -1,14 +1,59 @@
-const url = process.env.BASE_URL
+import { post, get, put, del } from 'axios';
+const url = 'https://crudcrud.com/api/eb80a8ac925946fea0594f4a72d8d7fa/todo';
 
-const createTodo = async () => {
-
+// Create
+const createTodo = async (title, desc, dateTime) => {
     const todo = {
-        title: "atitutlo"
+        title: title,
+        desc: desc,
+        dateTime: dateTime,
+        done: false
+    };
+
+    try {
+        const response = await post(url, todo);
+        console.log(response.data);
+    } catch(err) {
+        console.error(err);
     }
+}
 
-    try{
+// Read
+const readTodos = async () => {
+    try {
+        const response = await get(url);
+        console.log(response.data);
+    } catch(err) {
+        console.error(err);
+    }
+}
 
-    }catch(err){
-        console.log(err)
+// Read By ID
+const readById = async (id) => {
+    try {
+        const response = await get(`${url}/${id}`);
+        console.log('Deleted:', id);
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+// Update
+const updateTodo = async (id, updatedTodo) => {
+    try {
+        const response = await put(`${url}/${id}`, updatedTodo);
+        console.log(response.data);
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+// Delete
+const deleteTodo = async (id) => {
+    try {
+        const response = await del(`${url}/${id}`);
+        console.log('Deleted:', id);
+    } catch(err) {
+        console.error(err);
     }
 }
